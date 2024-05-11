091ccb3b44ae6e263c58f7f16ba3144fa1507962
lazzaric92 <lazzaric_92@hotmail.it>
2024-05-11 12:06:53 +0200

    saved ul element in a constant, created and appended li elements with for cycle

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
+const ulEl = document.querySelector('ul');
+
 for(let i = 1; i <= 100; i++){
+    const liEl = document.createElement('li');
     if(i % 3 == 0 && i % 5 !== 0){
         console.log('Fizz');
+        liEl.append('Fizz');
+        liEl.classList.add('fizz');
     } else if (i % 3 !== 0 && i % 5 == 0){
         console.log('Buzz');
+        liEl.append('Buzz');
+        liEl.classList.add('buzz');
     } else if (i % 3 == 0 && i % 5 == 0){
         console.log('FizzBuzz');
+        liEl.append('FizzBuzz');
+        liEl.classList.add('fizzbuzz');
     } else {
         console.log(i);
+        liEl.append(i);
     }
+
+    ulEl.appendChild(liEl);
 }
--- readme.md
+++ readme.md
@@ -14,5 +14,5 @@ Scrivo un ciclo for con indice che va da 1 a 100 (incluso) e che ad ogni ciclo:
         stampi l'indice.
 
 
-In HTML creo un main con dentro un container e all'interno un ul nel quale appenderò dei li con js.
+In HTML creo un main con dentro un container e all'interno un ul nel quale appenderò dei li creati con js.
 A seconda dell'indice js appenderà una stringa ai li, inoltre aggiungerà le classi 'fizz', 'buzz' e 'fizzbuzz' ai relativi li.
